cf1c31d16bcad696bae4a23a53f569807a60c845
shuvosvc <5arafatshovo@gmail.com>
2021-10-22 01:11:50 +0600

    feat: file filter

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 const express = require("express");
 const multer = require("multer");
+const path = require("path");
 const UPLOAD_FOLDER = "./Uploads";
+
+///////////////auto storage control
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, UPLOAD_FOLDER);
+//   },
+//   filename: (req, file, cb) => {
+//     const fileExt = path.extname(file.originalname);
+//     const fileName =
+//       file.originalname
+//         .replace(fileExt, "")
+//         .toLowerCase()
+//         .split(" ")
+//         .join("_") +
+//       "_" +
+//       Date.now();
+//     cb(null, fileName + fileExt);
+//   },
+// });
 
 let upload = multer({
   dest: UPLOAD_FOLDER,
+  //   storage: storage,
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.fieldname === "avatar") {
+      if (
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/jpeg"
+      ) {
+        cb(null, true);
+      } else {
+        cb(new Error("only .jpg,.png,jpeg format allowed"));
+      }
+    } else if (file.fieldname === "doc") {
+      if (file.mimetype === "application/pdf") {
+        cb(null, true);
+      } else {
+        cb(new Error("only .pdf formate allowed"));
+      }
+    } else {
+      cb(new Error("Unknown error"));
+    }
+  },
 });
 
 const app = express();
@@ -11,8 +57,8 @@ const app = express();
 app.post(
   "/",
   upload.fields([
-    { name: "avater", maxCount: 1 },
-    { name: "gallery", maxCount: 2 },
+    { name: "avatar", maxCount: 1 },
+    { name: "doc", maxCount: 2 },
   ]),
   (req, res) => {
     res.send("while world");
